4c21d9e48ae062f2d3088d9a8718e25d683a7c50
Vesko <veskoap@gmail.com>
2022-11-16 06:08:44 -0500

    [Ticket: X]
    [commit]
      template = ~/.gitmessage

--- components/About.tsx
+++ components/About.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 type Props = {}
 
-function About({}: Props) {
+export default function About({}: Props) {
   return (
-    <div>About</div>
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl 
+    px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking[20px] text-gray-500 text-2xl'>
+            About
+        </h3>
+    </div>
   )
 }
-
-export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,11 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-
-      <section id ='hero' className='snap-center'>
+      <section id ='hero' className='snap-start'>
         <Hero />
       </section>
-      {/* About */}
-      <section>
+      
+      <section id='about' className='snap-center'>
          <About />
       </section>
 
